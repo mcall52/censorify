6fcaea3ad60d3537a32921775c7de6479b24e642
Michael <ec2-user@ip-172-31-38-14.us-west-2.compute.internal>
2015-03-12 15:51:57 +0000

    City finder is works with comments section below as well. A few test js files were added as well for a quiz.

--- webServer.js
+++ webServer.js
@@ -1,12 +1,70 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var readline = require('url');
 var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
   var myRe = new RegExp("^"+urlObj.query["q"]);
   console.log(myRe);
-  if(urlObj.pathname.indexOf("getcity") != -1){
+  if(urlObj.pathname.indexOf("comment") != -1){
+    console.log("comment route");
+    if(req.method === "POST") {
+      console.log("POST comment route");  
+    
+    // First read the form data
+    var jsonData = "";
+    req.on('data', function (chunk) {
+      jsonData += chunk;
+    });
+    req.on('end', function () {
+      var reqObj = JSON.parse(jsonData);
+      console.log(reqObj);
+      console.log("Name: "+reqObj.Name);
+      console.log("Comment: "+reqObj.Comment);
+
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+        if(err){ 
+	  console.log('There was an ERROR');
+	  throw err;
+        }
+        db.collection('comments').insert(reqObj,function(err, records) {
+  	  console.log("Record added as " + records[0]._id);
+	  res.writeHead(200);
+	  res.end("");
+        });
+      });
+    });
+     
+    // Now put it into the database
+    /*var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+      if(err) throw err;
+      db.collection('comments').insert(reqObj,function(err, records) {
+	console.log("Record added as " + records[0]._id);
+      });
+    });*/
+    }else if(req.method === "GET"){
+      console.log("In GET");
+      var MongoClient = require('mongodb').MongoClient;
+      MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+	if(err) throw err;
+	db.collection("comments", function(err, comments){
+	  if(err) throw err;
+	  comments.find(function(err, items){
+	    items.toArray(function(err, itemArr){
+	      console.log("Document Array: ");
+	      console.log(itemArr);
+	      res.writeHead(200);
+	      res.end(JSON.stringify(itemArr));
+	    });
+	  });
+	});
+      });
+    }
+  }
+  else if(urlObj.pathname.indexOf("getcity") != -1){
     var myRe = new RegExp("^"+urlObj.query["q"]);
     console.log(myRe);
     fs.readFile('html/cities.dat.txt', function (err, data) {
@@ -28,6 +86,7 @@ http.createServer(function (req, res) {
   }else{
     fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
       if (err) {
+	console.log('ERROR 404');
         res.writeHead(404);
         res.end(JSON.stringify(err));
         return;
